d448329b578e8a7ef18722f56163b8fe6756204c
SujalKumar10 <146089340+SujalKumar10@users.noreply.github.com>
2024-06-21 15:57:27 +0530

    Update MyWallet.sol

--- MyWallet.sol
+++ MyWallet.sol
@@ -4,36 +4,33 @@ pragma solidity ^0.8.9;
 contract MyWallet{
     mapping(address => uint) private balances;
     mapping(address => bool) private account;
-    mapping(address => uint) private WalletID;
+    mapping(address => uint) private Wallet;
 
-    modifier walletID(){
+    modifier IDchecker(){
         require(account[msg.sender] == true,"Enter Wallet ID");
         _;
     }
 
     function Wallet_ID(uint ID) public {
-        WalletID[msg.sender] = ID;
+        Wallet[msg.sender] = ID;
         account[msg.sender] = true;
     }
 
-    function deposit(uint Amount) public walletID{
+    function deposit(uint Amount) public IDchecker(){
         assert(balances[msg.sender] + Amount>= Amount);
         balances[msg.sender]+= Amount;
     }
 
-    function withdraw(uint amount) public walletID{
+    function withdraw(uint amount) public IDchecker(){
         require(amount <= balances[msg.sender], "Insufficient Balance");
-        balances[msg.sender] -= amount;
-        
+        balances[msg.sender] -= amount;    
     }
 
     function CheckBalance(uint ID) public view returns(uint){
-        if(ID != WalletID[msg.sender]){
+        if(ID != Wallet[msg.sender]){
             revert("Incorrct ID");
         }
         return balances[msg.sender];
 
     }
-
-
 }
